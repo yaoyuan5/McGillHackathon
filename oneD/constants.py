1b059a3b3989c55f733c73734e799193fcb4e51a
yaoyuan5 <yy.yao@mail.utoronto.ca>
2016-07-09 11:29:29 -0400

    Update constants.py

---
+++ oneD/constants.py
@@ -0,0 +1,19 @@
+#this contains the fundamental set up
+
+
+import sys
+import time
+from numpy import *
+from pylab import *
+from matplotlib import animation
+
+
+dx=0.01
+dt
+hbar=0.1
+m=1.
+#N denotes the number of time steps forward
+N=500
+L_x=2. # L_x, in setting up the x axis, the x axis runs from [-Lx,Lx]
+x_ax=arange(-L_x,L_x,dx)
+time=arange(0,N*dt,dt)
